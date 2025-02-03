3d5cf1d16a59c599cf3f10d26de761ddf51810d3
Rebzyyx <exesqontopp@gmail.com>
2025-02-03 18:23:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createAudioPlayer, createAudioResource, joinVoiceChannel, AudioPlayerSta
 const ytdl = require('ytdl-core');
 const axios = require('axios');
 
-const TOKEN = ('MTMzNTQyMDM2Mjk4ODQ1Mzk0MA.GHnB11.KQEAQPm72VluOoh1FqdfcS6O3GetYX0PNrWH5A');
+const TOKEN = ('MTMzNTQyMDM2Mjk4ODQ1Mzk0MA.GmltL3.60-Js3_TnPFlgdbXwtwcbScZUrPSimW6wyUz7I');
 const CLIENT_ID = '1335420362988453940';
 
 if (!TOKEN || !CLIENT_ID) {
